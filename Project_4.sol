966d20ac1f3431ba3bfe86b639255c2d56449d14
jeeva328 <62882566+jeeva328@users.noreply.github.com>
2020-07-28 23:06:57 +0530

    Add files via upload

---
+++ Project_4.sol
@@ -0,0 +1,63 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+contract student{
+    
+string public name;
+   
+string public roll;
+    
+string public mark1;
+    
+string public mark2;
+    
+string public mark3;
+    
+string public mark4;
+    
+string public batch;
+    
+string public status;
+     
+}
+function stu(string newname, string newroll, string newmark1, string newmark2, string newmark3, string newmark3, string newmark4, string newbatch)public{
+        
+ name = newname;
+        
+ roll = newroll;
+      
+   mark1 = newmark1;
+   
+      mark2 = newmark2;
+       
+  mark3 = newmark3;
+         
+mark4 = newmark4;
+        
+ batch = newbatch;
+        
+ string total = mark1 + mark2 + mark3 + mark4;
+         
+string percent = total/100;
+         
+if(percent>30){
+            
+ status = "pass";
+         
+}else{
+            
+ status = "fail";
+     
+         
+}
+         
+return batch;
+         
+}
+    
+ function getdetials() public view returns(string,string,string){
+        
+ return(name,roll,status);
+    
+ }
+     
+}
